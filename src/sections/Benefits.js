f3c0bccebd3f92fb1741021791e3abb591ab8d81
Fabio <fabioac71@gmail.com>
2021-01-28 17:05:40 -0300

    final changes, I think

--- src/sections/Benefits.js
+++ src/sections/Benefits.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
 const Benefits = () => {
+
+   const [scrollAmount, setScrollAmount] = useState(0);
+
+   const scrollHandler = (e) => {
+      const element = e.target;
+      if (element.classList.contains('right-box') || element.classList.contains('left-box')){
+         setScrollAmount(element.scrollTop);
+      }
+   }
+
+   useEffect(() => {
+       const rightBox = document.querySelector('.right-box') || document.querySelector('.left-box') ;
+       rightBox.scrollTop = scrollAmount;
+   }, [scrollAmount])
     
-    const scrollHandlerRight = (e) => {
-      const element = e.target;
-      const elementClasses = element.classList;
-      if(elementClasses.contains('scrollable') && elementClasses.contains('right-box')){
-         const leftBox = document.querySelector('.left-box');
-         leftBox.scrollTop = element.scrollTop;
-      }
-   }
+   //  const scrollHandlerRight = (e) => {
+   //    const element = e.target;
+   //    const elementClasses = element.classList;
+   //    if(elementClasses.contains('scrollable') && elementClasses.contains('right-box')){
+   //       const leftBox = document.querySelector('.left-box');
+   //       leftBox.scrollTop = element.scrollTop;
+   //    }
+   // }
 
-   const scrollHandlerLeft = (e) => {
-      const element = e.target;
-      if(element.classList.contains('scrollable')){
-         const rightBox = document.querySelector('.right-box');
-         rightBox.scrollTop = element.scrollTop;
-      }
-   }
+   // const scrollHandlerLeft = (e) => {
+   //    const element = e.target;
+   //    const elementClasses = element.classList;
+   //    if(elementClasses.contains('scrollable') && elementClasses.contains('left-box')){
+   //       const rightBox = document.querySelector('.right-box');
+   //       rightBox.scrollTop = element.scrollTop;
+   //    }
+   // }
 
-   const toggleHandler = (e) => {
-      const element = e.target.parentNode;
-      if (element.classList.contains('right-box')){
-         element.classList.toggle('scrollable');
-      }else if(element.classList.contains('left-box')){
-         element.classList.toggle('scrollable');
-      }
-   }
+   // const toggleHandler = (e) => {
+   //    const element = e.target.parentNode;
+   //    if (element.classList.contains('right-box')){
+   //       element.classList.toggle('scrollable');
+   //    }else if(element.classList.contains('left-box')){
+   //       element.classList.toggle('scrollable');
+   //    }
+   // }
 
 
    return (
@@ -38,19 +53,35 @@ const Benefits = () => {
              </Text>
           </Title>
           <CenterBox>
-            <BoxLeft onMouseOver={toggleHandler} onMouseLeave={toggleHandler} onScroll={scrollHandlerLeft} className='left-box'>
-               <p>Ajudar a regular o açúcar no sangue, já que aumenta a sensibilidade à insulina, um hormônio responsável pelo controle da glicose no sangue, e, por isso, poderia ter benefícios para as pessoas que possuem diabetes e resistência à insulina;
+            <BoxLeft 
+            onScroll={scrollHandler} 
+            className='left-box'>
+               <p>Ajudar a regular o açúcar no sangue, já que aumenta a sensibilidade à insulina, um hormônio responsável pelo controle da glicose no sangue, e, por isso, poderia ter benefícios para as pessoas que possuem diabetes e resistência à insulina</p>
                <br/>
                <br/>
-               <br/>
-               <br/>
-               <br/>
-               <br/>
-               Manter a saúde do coração, já que foi demonstrado em alguns estudos, que o picolinato de cromo ajuda a regular os níveis de colesterol e triglicerídeos, diminuindo o risco de formação das placas ateromatosas e, consequentemente, o risco de desenvolver doenças cardíacas, principalmente em pessoas diabéticas. Apesar disso, esse mecanismo ainda não está totalmente claro;
+               <p>
+               Manter a saúde do coração, já que foi demonstrado em alguns estudos, que o picolinato de cromo ajuda a regular os níveis de colesterol e triglicerídeos, diminuindo o risco de formação das placas ateromatosas e, consequentemente, o risco de desenvolver doenças cardíacas, principalmente em pessoas diabéticas. Apesar disso, esse mecanismo ainda não está totalmente claro
               </p>
+              <br/>
+              <br/>
+              <p>O picolinato de cromo pode ter muitos resultados benéficos quando suplementado. Realça o efeito da insulina no corpo, melhorando o recebimento da glicose, desse modo causando uma melhor circulação do sangue e manutenção dos níveis de açúcar no sangue</p>
+              <br/>
+              <br/>
+              <p>Cassia Angustifolia é constituído por extrato seco de Sene (Cassia Angustifolia), o qual tem como constituintes predominantes os glicosídeos hidroxiantracênicos, calculados como senosídeo B. O Sene tem um efeito laxativo (atividade catártica), devido à ação dos senosídeos e seus metabólitos ativos no cólon</p>
             </BoxLeft>
-            <BoxRight onMouseOver={toggleHandler} onMouseLeave={toggleHandler} onScroll={scrollHandlerRight} className='right-box' >
-                <p>Favorecer a perda de peso, já que pode também interferir no metabolismo dos carboidratos, gorduras e proteínas. No entanto, os resultados sobre esse benefício ainda não são conclusivos, já que indicam que a perda de peso não foi significativa</p>
+            <BoxRight 
+            onScroll={scrollHandler} 
+            className='right-box' >
+                <p>Favorecer a perda de peso, já que pode também interferir no metabolismo dos carboidratos, gorduras e proteínas. No entanto, os resultados sobre esse benefício ainda não são conclusivos, já que indicam que a perda de peso não foi significativa.</p>
+                <br/>
+                <br/>
+                <p>Estudos demonstraram uma redução da gordura corporal com concomitante aumento da massa muscular em indivíduos tratados com picolinato de cromo. Picolinato de cromo é um suplemento dietético que pode ser útil em inibir o apetite. Adicionalmente, tem demonstrado um crescimento no músculo e ajuste da gordura simultaneamente.</p>
+                <br/>
+                <br/>
+                <p>Picolinato de cromo tem uma eficiência de absorção até 10 vezes superior ao cromo encontrado naturalmente em dietas ou sais inorgânicos de cromo. No primeiro estudo nutricional divulgado em 1989, voluntários que tomaram 200 microgramas/dia de cromo na forma de picolinato mostraram diminuição nos níveis totais de colesterol e LDL colestero.</p>
+                <br/>
+                <br/>
+                <p>Picolinato de cromo é conhecido como a melhor fonte de cromo. O cromo é um mineral que é essencial para a produção de insulina do corpo. Pesquisas mostram que o cromo é uma parte essencial de nossa dieta. </p>
             </BoxRight>
           </CenterBox>
         </StyledSection>
@@ -96,7 +127,7 @@ const BoxLeft = styled.div`
     padding-right: 0.7rem;
     text-align: justify;
     &::-webkit-scrollbar{
-       width: 0.3rem;
+       width: 0.5rem;
        display: block;
     
     }
